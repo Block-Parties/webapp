d7e54c72cf4eedcaa4bf5316927fce7d1b942554
Andres Gutierrez <andresgutierrez535@gmail.com>
2021-09-09 21:33:13 -0400

    landing page carousel

---
+++ src/lib/components/landing/Carousel.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+    let activeIndex = 0
+
+    function showNext() {}
+
+    function showPrev() {}
+</script>
+
+<div class="outer">
+    <div class="arrow" on:click={showPrev}>
+        <img src="/images/arrow_left.svg" alt="previous" />
+    </div>
+
+    <div class="content">
+        <slot />
+    </div>
+
+    <div class="arrow" on:click={showPrev}>
+        <img src="/images/arrow_right.svg" alt="previous" />
+    </div>
+</div>
+
+<div class="dots">
+    <div class="dot {activeIndex == 0 ? 'active' : ''}" />
+    <div class="dot {activeIndex == 1 ? 'active' : ''}" />
+    <div class="dot {activeIndex == 2 ? 'active' : ''}" />
+</div>
+
+<style lang="scss">
+    .outer {
+        display: flex;
+        align-items: center;
+    }
+
+    .content {
+        margin: 0 32px;
+    }
+
+    .dots {
+        display: flex;
+        justify-content: center;
+        margin-top: 64px;
+
+        .dot {
+            transition: all 0.3s;
+
+            margin: 0 16px;
+            width: 16px;
+            height: 16px;
+            border-radius: 8px;
+
+            background: #d7d4e7;
+
+            &.active {
+                background: #170d4d;
+            }
+        }
+    }
+</style>
